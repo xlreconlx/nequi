bf57013906232a2fcd06acc456ea0c09b7e84168
andersonur <andersonur@hotmail.com>
2025-01-28 18:06:56 -0500

    Configuracion de proyecto

---
+++ src/main/java/com/nequi/prueba/entity/Franquicia.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nequi.prueba.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author ander
+ */
+@Entity
+@Table(name = "franquia")
+public class Franquicia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idFranquicia")
+    private Long idFranquicia;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    public Long getIdFranquicia() {
+        return idFranquicia;
+    }
+
+    public void setIdFranquicia(Long idFranquicia) {
+        this.idFranquicia = idFranquicia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
---
+++ src/main/java/com/nequi/prueba/repository/FranquiciaRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nequi.prueba.repository;
+
+import com.nequi.prueba.entity.Franquicia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author ander
+ */
+public interface FranquiciaRepository extends JpaRepository<Franquicia, Long>{
+}
